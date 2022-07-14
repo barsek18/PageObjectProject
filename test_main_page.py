79183324a9b9b167613f71a27e31b63dc32bd841
Admin <jseti18@gmail.com>
2022-07-14 10:50:57 +0300

    #added method to catch exceptions base_page.is_element_present(how, what)

--- test_main_page.py
+++ test_main_page.py
@@ -9,3 +9,12 @@ def test_guest_can_go_to_login_page(browser):
     page.open()
     # выполняем метод страницы — переходим на страницу логина
     page.go_to_login_page()
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page = MainPage(browser, link)
+    # открываем страницу
+    page.open()
+    # выполняем метод страницы — переходим на страницу логина
+    page.should_be_login_link()
